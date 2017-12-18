21d9f32458362295bd9ef01f5aa7771ab031e45c
kinjo yuji <kinjoyuji@gmail.com>
2017-12-18 15:50:31 +0900

    ログイン実装

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,10 +4,23 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { setEmail, setPassword } from '../actions/index';
 import { Container, Header, Content, Form, Item, Input, Left, Right, Body, Title, Button, Text } from 'native-base';
+import firebase from 'firebase';
 
 export class LoginScreen extends Component {
   constructor(props){
     super(props);
+  }
+
+  handleSubmit(){
+    console.log(this.props);
+    firebase.auth().signInWithEmailAndPassword(this.props.email, this.props.password)
+      .then((user) => {
+        console.log("success", user);
+        this.props.navigation.navigate('ToDo');
+      })
+      .catch((error) => {
+        console.log("firebase error", error);
+      });
   }
 
   render() {
@@ -28,9 +41,9 @@ export class LoginScreen extends Component {
                 secureTextEntry={true}
               />
             </Item>
-            <Button block info onPress={() => this.props.navigation.navigate('ToDo')}>
+            <Button block info onPress={this.handleSubmit.bind(this)}>
               <Text>
-                送信
+                ログイン
               </Text>
             </Button>
           </Form>
